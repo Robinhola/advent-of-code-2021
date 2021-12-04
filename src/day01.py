c764aa6aac2c09fcbfc08ed7f3a08b6792e9d09f
Robin Elrharbi--Fleury <robinef@Robins-MacBook-Pro.local>
2021-12-04 14:31:05 +0000

    Day 1

---
+++ src/day01.py
@@ -0,0 +1,22 @@
+from day01_data import data
+
+def part1():
+    count = 0
+    for i in range(len(data)):
+        if i == 0: continue
+        a, b = data[i - 1], data[i]
+        if a < b: count += 1
+    return count
+
+def part2():
+    count = 0
+    for i in range(len(data)):
+        a = sum((data[i - 3], data[i - 2], data[i -1]))
+        b = sum((data[i - 2], data[i - 1], data[i]))
+        if a < b: count += 1
+    return count
+
+
+if __name__ == "__main__":
+    print(part1())
+    print(part2())
