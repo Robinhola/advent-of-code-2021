c3ba646c285ad166af5cd46d2c16289a2b6c951e
Robin Elrharbi-Fleury <robinef@palantir.com>
2021-12-07 13:04:08 +0000

    Functional day01

--- adventofcode2021/days01to05/day01.py
+++ adventofcode2021/days01to05/day01.py
@@ -1,27 +1,22 @@
+from typing import Generator
 from adventofcode2021.input_data import day01 as data
 
 
-def part1():
-    count = 0
-    for i in range(len(data)):
-        if i == 0:
-            continue
-        a, b = data[i - 1], data[i]
-        if a < b:
-            count += 1
-    return count
+def is_increasing(i: int):
+    a, b = data[i - 1], data[i]
+    return i > 0 and b > a
 
 
-def part2():
-    count = 0
-    for i in range(len(data)):
-        a = sum((data[i - 3], data[i - 2], data[i - 1]))
-        b = sum((data[i - 2], data[i - 1], data[i]))
-        if a < b:
-            count += 1
-    return count
+def is_sum_increasing(i: int):
+    a = sum((data[i - 3], data[i - 2], data[i - 1]))
+    b = sum((data[i - 2], data[i - 1], data[i]))
+    return i > 2 and b > a
+
+
+def count_values_based_on(predicate: callable):
+    return len(tuple(filter(predicate, range(len(data)))))
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    print(count_values_based_on(is_increasing))
+    print(count_values_based_on(is_sum_increasing))
