05d833f1b5b28f14045edb031fdf6517358fa765
Robin Elrharbi-Fleury <robinef@palantir.com>
2021-12-09 11:11:02 +0000

    Prettier day09

--- adventofcode2021/days06to10/day09.py
+++ adventofcode2021/days06to10/day09.py
@@ -4,6 +4,8 @@ from adventofcode2021.input_data import day09 as raw_data
 
 
 data = [[int(x) for x in l] for l in raw_data.splitlines()]
+LEN_X = len(data[0])
+LEN_Y = len(data)
 
 
 def access_data(x, y):
@@ -14,66 +16,51 @@ def form_coordinates(x, y):
     return (x, y)
 
 
-def add_if_valid(x, y, action: callable, container: list):
-    if 0 <= x < len(data[0]) and 0 <= y < len(data):
-        container.append(action(x, y))
-    return container
+def is_valid(x, y):
+    return 0 <= x < LEN_X and 0 <= y < LEN_Y
 
 
-def find_neighbours(x, y, action_on_x_y: callable):
-    neighbours = []
-    neighbours = add_if_valid(x - 1, y + 0, action_on_x_y, neighbours)
-    neighbours = add_if_valid(x + 1, y + 0, action_on_x_y, neighbours)
-    neighbours = add_if_valid(x + 0, y + 1, action_on_x_y, neighbours)
-    neighbours = add_if_valid(x + 0, y - 1, action_on_x_y, neighbours)
-    return neighbours
-
-
-def find_low_points():
-    def is_low_point(x, y):
-        return data[y][x] < min(find_neighbours(x, y, access_data))
-
+def find_neighbours(x, y, action_on_x_and_y: callable):
     return [
-        (x, y)
-        for x in range(len(data[0]))
-        for y in range(len(data))
-        if is_low_point(x, y)
+        action_on_x_and_y(*coord)
+        for coord in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+        if is_valid(*coord)
     ]
 
 
+def is_low_point(x, y):
+    return access_data(x, y) < min(find_neighbours(x, y, access_data))
+
+
+def find_low_points():
+    return [(x, y) for x in range(LEN_X) for y in range(LEN_Y) if is_low_point(x, y)]
+
+
 def find_basin(coord: tuple, seen: set):
-    x, y = coord
-    basin = {coord}
-    to_visit = [c for c in find_neighbours(x, y, form_coordinates)]
-
+    basin = set()
+    to_visit = [coord]
     while to_visit:
-        x, y = c = to_visit.pop()
-
-        if access_data(x, y) == 9 or c in seen:
-            continue
-
-        basin.add(c)
-        seen.add(c)
-
-        for c in find_neighbours(x, y, form_coordinates):
-            to_visit.append(c)
-
+        coord = to_visit.pop()
+        seen.add(coord)
+        basin.add(coord)
+        to_visit.extend(
+            new_coord
+            for new_coord in find_neighbours(*coord, form_coordinates)
+            if access_data(*new_coord) != 9 and new_coord not in seen
+        )
     return basin
 
 
 def part1():
-    low_point_values = (data[y][x] for (x, y) in find_low_points())
-    return sum(x + 1 for x in low_point_values)
+    low_point_values = (access_data(*coord) for coord in find_low_points())
+    risk_values = (x + 1 for x in low_point_values)
+    return sum(risk_values)
 
 
 def part2():
     low_points = find_low_points()
-    seen = set(low_points)
-    basins = list()
-
-    for coord in low_points:
-        basins.append(find_basin(coord, seen))
-
+    already_seen = set()
+    basins = (find_basin(coord, already_seen) for coord in low_points)
     largest_basins = sorted(len(x) for x in basins)[-3:]
     return math.prod(largest_basins)
 
