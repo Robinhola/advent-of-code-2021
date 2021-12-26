21f04ad7af9d3f06e95cce0fe91ef4a486b1a012
Robin Elrharbi--Fleury <robin.elrharbifleury@gmail.com>
2021-12-26 21:36:16 +0000

    Day18 second try

--- adventofcode2021/days16to20/day18.py
+++ adventofcode2021/days16to20/day18.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
-from os import defpath
-import re
 from types import new_class
-from typing import Optional, Union
+from typing import List
 import math
+import itertools
 
 raw_data = """[1,1]
 [2,2]
@@ -16,176 +15,122 @@ raw_data = """[1,1]
 [4,4]
 [5,5]"""
 
-# raw_data = """[[[[4,3],4],4],[7,[[8,4],9]]]
-# [1,1]"""
-
-raw_data = """[1,1]
-[2,2]
-[3,3]
-[4,4]
-[5,5]
-[6,6]"""
-
-raw_data = """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-[7,[5,[[3,8],[1,4]]]]
-[[2,[2,2]],[8,[8,1]]]
-[2,9]
-[1,[[[9,3],9],[[9,0],[0,7]]]]
-[[[5,[7,4]],7],1]
-[[[[4,2],2],6],[8,7]]"""
-
 
 @dataclass
-class SnailFishNumber:
-    left: Optional["SnailFishNumber"]
-    right: Optional["SnailFishNumber"]
+class SFNumber:
+    index: int
+    value: int
+    position: bool  # False for Left, True for Right
     depth: int
-    parent: Optional["SnailFishNumber"] = None
-    value: Optional[int] = None
 
-    @classmethod
-    def addition(cls, a, b):
-        a.increase_depth()
-        b.increase_depth()
-        sum = cls(a, b, 0)
-        a.parent = sum
-        b.parent = sum
 
-        to_explode, to_split = sum.look_for_node(4)
-        while to_explode is not None or to_split is not None:
-            if to_explode:
-                cls.explode_node(to_explode)
-            if to_split:
-                cls.split_node(to_split)
-            to_explode, to_split = sum.look_for_node(4)
+def parse_line(line: str):
+    numbers = []
+    depth = 0
+    is_right = []
+    for character in line:
+        if character == "[":
+            depth += 1
+            is_right.append(False)
+        elif character == "]":
+            depth -= 1
+            is_right.pop()
 
-        return sum
+        elif character.isnumeric():
+            index = len(numbers)
+            numbers.append(SFNumber(index, int(character), is_right[-1], depth))
+        elif character == ",":
+            is_right[-1] = True
+    return numbers
 
-    @classmethod
-    def explode_node(cls, node: "SnailFishNumber"):
-        if node.find_left() is not None:
-            node.find_left().value += node.left.value
-        if node.find_right() is not None:
-            node.find_right().value += node.right.value
 
-        if node.parent.left is node:
-            node.parent.left = cls(None, None, node.depth, node.parent, 0)
-        elif node.parent.right is node:
-            node.parent.right = cls(None, None, node.depth, node.parent, 0)
+def is_left(x: SFNumber):
+    return x.position is False
 
-    @classmethod
-    def split_node(cls, node: "SnailFishNumber"):
-        value = node.value
-        depth = node.depth
-        node.left = cls(None, None, depth + 2, node, math.floor(value / 2.0))
-        node.right = cls(None, None, depth + 2, node, math.ceil(value / 2.0))
-        node.value = None
-        node.depth = depth + 1
 
-    def look_for_node(self, depth):
-        if self.value and self.value > 9:
-            return None, self
+def is_right(x: SFNumber):
+    return x.position
 
-        if self.value is None and self.depth >= depth:
-            if self.left.value is None:
-                return self.left.look_for_node(depth)
-            if self.right.value is None:
-                return self.right.look_for_node(depth)
-            return self, None
 
-        if self.left:
-            a, b = self.left.look_for_node(depth)
-            if a is not None or b is not None:
-                return a, b
+def needs_work(numbers: List[SFNumber]):
+    for x in numbers:
+        if x.index >= len(numbers) - 1:
+            break
 
-        if self.right:
-            a, b = self.right.look_for_node(depth)
-            if a is not None or b is not None:
-                return a, b
+        y = numbers[x.index + 1]
 
-        return None, None
+        if is_left(x) and is_right(y) and x.depth > 4:
+            return x
+        elif x.value > 9:
+            return x
+        elif y.value > 9:
+            return y
 
-    def increase_depth(self):
-        self.depth += 1
-        for x in (self.left, self.right):
-            if x.value is None:
-                x.increase_depth()
+    return None
 
-    @classmethod
-    def from_input(cls, string, depth=0, parent=None):
-        middle, opened = None, 0
-        for i, c in enumerate(string):
-            if c == "[":
-                opened += 1
-            elif c == "]":
-                opened -= 1
-            elif c == "," and opened == 1:
-                middle = i
-                break
 
-        if middle is None:
-            return cls(None, None, depth, parent, int(string))
+def explode(x: SFNumber, numbers: List[SFNumber]):
+    left = x
+    right = numbers[x.index + 1]
 
-        new_node = cls(None, None, depth, parent)
+    left_numbers = numbers[: left.index]
+    right_numbers = numbers[right.index + 2 :]
 
-        new_node.left = cls.from_input(string[1:middle], depth + 1, new_node)
-        new_node.right = cls.from_input(
-            string[middle + 1 : len(string) - 1], depth + 1, new_node
-        )
+    if left_numbers:
+        left_numbers[-1].value += left.value
 
-        return new_node
+    if right_numbers:
+        right_numbers[0].value += right.value
 
-    def right_most(self):
-        if self.value is not None:
-            return self
-        else:
-            return self.right.right_most()
+    for n in right_numbers:
+        n.index -= 1
 
-    def left_most(self):
-        if self.value is not None:
-            return self
-        else:
-            return self.left.left_most()
+    should_be_right = False
+    if left_numbers:
+        should_be_right = left_numbers[-1].position
 
-    def find_left(self):
-        if not self.parent:
-            return None
-        if self.parent.left is self:
-            return self.parent.find_left()
-        return self.parent.left.right_most()
+    new_number = SFNumber(x.index, 0, should_be_right, x.depth - 1)
 
-    def find_right(self):
-        if not self.parent:
-            return None
-        if self.parent.right is self:
-            return self.parent.find_right()
-        return self.parent.right.left_most()
+    return left_numbers + [new_number] + right_numbers
 
-    def __str__(self):
-        if self.value is not None:
-            return str(self.value)
-        # return f"[{str(self.left)},{str(self.right)}]({self.depth})"
-        return f"[{str(self.left)},{str(self.right)}]"
 
-    def __add__(self, other):
-        return SnailFishNumber.addition(self, other)
+def split(x: SFNumber, numbers: List[SFNumber]):
+    new_left = SFNumber(x.index, math.floor(x.value / 2.0), False, x.depth + 1)
+    new_right = SFNumber(x.index, math.ceil(x.value / 2.0), True, x.depth + 1)
+
+    left_numbers = numbers[: x.index]
+    right_numbers = numbers[x.index + 1 :]
+
+    for n in right_numbers:
+        n.index += 1
+
+    return left_numbers + [new_left, new_right] + right_numbers
+
+
+def addition(a: List[SFNumber], b: List[SFNumber]):
+    for x in itertools.chain(a, b):
+        x.depth += 1
+
+    result = a + b
+
+    x = needs_work(result)
+    while x:
+        if x.depth >= 4:
+            result = explode(x, result)
+        elif x.value > 9:
+            result = split(x, result)
+        x = needs_work(result)
+
+    return result
 
 
 def part1():
-    a = SnailFishNumber.from_input("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]")
-    b = SnailFishNumber.from_input("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]")
-    return a + b
-
     number = None
     for l in raw_data.splitlines():
         if number == None:
-            number = SnailFishNumber.from_input(l)
+            number = parse_line(l)
         else:
-            number = number + SnailFishNumber.from_input(l)
-        print(number)
+            number = addition(number, parse_line(l))
     return number
 
 
